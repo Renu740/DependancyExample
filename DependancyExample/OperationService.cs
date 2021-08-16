3208f8aaed00b9747dd625cec0a01979ffdef057
Renu740 <Renu.Yadav@Ltts.com>
2021-08-16 17:02:40 +0530

    initial commit

---
+++ DependancyExample/OperationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependancyExample
+{
+    public class OperationService : IScopedService, ISingletonService, ITransientService
+    {
+        Guid id;
+        public OperationService()
+        {
+            id = Guid.NewGuid();
+        }
+        public Guid GetOperationID()
+        {
+            return id;
+        }
+    }
+}
